f700bd7da3bf07353602fad46177b51044601185
hubbylei <hubbylei@hubbylei.dynu.net>
2023-10-25 18:28:19 +0800

    Initial commit

---
+++ diy-part1.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part1.sh
+# Description: OpenWrt DIY script part 1 (Before Update feeds)
+#
+
+# Uncomment a feed source
+#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+
+# Add a feed source
+#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+git clone https://github.com/pymumu/openwrt-smartdns package/custom/smartdns
+git clone https://github.com/pymumu/luci-app-smartdns -b lede package/custom/luci-app-smartdns
+git clone https://github.com/xiaorouji/openwrt-passwall -b main package/custom/luci-app-passwall
+git clone https://github.com/xiaorouji/openwrt-passwall-packages -b main package/custom/openwrt-passwall
+git clone https://github.com/tty228/luci-app-wechatpush -b master package/custom/luci-app-wechatpush
+git clone https://github.com/hubbylei/openssl package/custom/openssl
---
+++ diy-part2.sh
@@ -0,0 +1,92 @@
+#!/bin/bash
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part2.sh
+# Description: OpenWrt DIY script part 2 (After Update feeds)
+#
+
+# Modify default IP
+#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+
+del_data="
+./feeds/luci/applications/luci-app-passwall
+./feeds/luci/applications/luci-app-wechatpush
+./feeds/luci/applications/luci-app-smartdns
+./feeds/luci/applications/luci-app-serverchan
+./feeds/packages/net/brook
+./feeds/packages/net/dns2socks
+./feeds/packages/net/microsocks
+./feeds/packages/net/pdnsd-alt
+./feeds/packages/net/v2ray-geodata
+./feeds/packages/net/naiveproxy
+./feeds/packages/net/shadowsocks-rust
+./feeds/packages/net/shadowsocksr-libev
+./feeds/packages/net/simple-obfs
+./feeds/packages/net/sing-box
+./feeds/packages/net/tcping
+./feeds/packages/net/trojan
+./feeds/packages/net/trojan-go
+./feeds/packages/net/trojan-plus
+./feeds/packages/net/v2ray-core
+./feeds/packages/net/v2ray-plugin
+./feeds/packages/net/xray-plugin
+./feeds/packages/net/chinadns-ng
+./feeds/packages/net/dns2tcp
+./feeds/packages/net/tcping
+./feeds/packages/net/hysteria
+./feeds/packages/net/tuic-client
+./feeds/packages/devel/gn
+./feeds/packages/net/smartdns
+./feeds/packages/net/ipt2socks
+./feeds/packages/net/xray-core
+./feeds/packages/lang/golang
+./package/libs/openssl
+./package/network/config/firewall
+./package/network/utils/iptables
+"
+
+for cmd in $del_data;
+do
+	rm -rf $cmd
+	echo "Deleted $cmd"
+done
+
+cp -rf ../golang feeds/packages/lang/
+rm -rf package/kernel/linux/modules/netfilter.mk
+cp -rf ../modules/netfilter.mk package/kernel/linux/modules/netfilter.mk
+cp -rf ../custom/* package/custom/
+rm -rf target/linux/mediatek/patches-5.4/0504-macsec-revert-async-support.patch
+sed -i 's/LINUX_VERSION-5.4 =.*/LINUX_VERSION-5.4 = .258/g' include/kernel-5.4
+sed -i 's/LINUX_KERNEL_HASH.*/LINUX_KERNEL_HASH-5.4.258 = 39701815a7a1b5ea4253db18e9327a45926a944499628d032da63c1bb6ea623e/g' include/kernel-5.4
+
+sed -i '/sed -r -i/a\\tsed -i "s,#Port 22,Port 22,g" $(1)\/etc\/ssh\/sshd_config\n\tsed -i "s,#ListenAddress 0.0.0.0,ListenAddress 0.0.0.0,g" $(1)\/etc\/ssh\/sshd_config\n\tsed -i "s,#PermitRootLogin prohibit-password,PermitRootLogin yes,g" $(1)\/etc\/ssh\/sshd_config' feeds/packages/net/openssh/Makefile
+sed -i 's/;Listen = 0.0.0.0:1688/Listen = 0.0.0.0:1688/g' feeds/packages/net/vlmcsd/files/vlmcsd.ini
+
+GEOIP_VER=$(echo -n `curl -sL https://api.github.com/repos/Loyalsoldier/v2ray-rules-dat/releases/latest | jq .tag_name | sed 's/\"//g'`)
+GEOIP_HASH=$(echo -n `curl -sL https://github.com/Loyalsoldier/v2ray-rules-dat/releases/download/$GEOIP_VER/geoip.dat.sha256sum | awk '{print $1}'`)
+GEOSITE_VER=$GEOIP_VER
+GEOSITE_HASH=$(echo -n `curl -sL https://github.com/Loyalsoldier/v2ray-rules-dat/releases/download/$GEOSITE_VER/geosite.dat.sha256sum | awk '{print $1}'`)
+sed -i '/HASH:=/d' package/custom/openwrt-passwall/v2ray-geodata/Makefile
+
+sed -i 's/GEOIP_VER:=.*/GEOIP_VER:='"$GEOIP_VER"'/g' package/custom/openwrt-passwall/v2ray-geodata/Makefile
+sed -i 's/https:\/\/github.com\/v2fly\/geoip/https:\/\/github.com\/Loyalsoldier\/v2ray-rules-dat/g' package/custom/openwrt-passwall/v2ray-geodata/Makefile
+sed -i '/FILE:=$(GEOIP_FILE)/a\\tHASH:='"$GEOIP_HASH"'' package/custom/openwrt-passwall/v2ray-geodata/Makefile
+
+sed -i 's/https:\/\/github.com\/v2fly\/domain-list-community/https:\/\/github.com\/Loyalsoldier\/v2ray-rules-dat/g' package/custom/openwrt-passwall/v2ray-geodata/Makefile
+sed -i 's/GEOSITE_VER:=.*/GEOSITE_VER:='"$GEOSITE_VER"'/g' package/custom/openwrt-passwall/v2ray-geodata/Makefile
+sed -i 's/dlc.dat/geosite.dat/g' package/custom/openwrt-passwall/v2ray-geodata/Makefile
+sed -i '/FILE:=$(GEOSITE_FILE)/a\\tHASH:='"$GEOSITE_HASH"'' package/custom/openwrt-passwall/v2ray-geodata/Makefile
+
+sed -i 's/URL:=https:\/\/www.v2fly.org/URL:=https:\/\/github.com\/Loyalsoldier\/v2ray-rules-dat/g' package/custom/openwrt-passwall/v2ray-geodata/Makefile
+
+SMARTDNS_VER=$(echo -n `curl -sL https://api.github.com/repos/pymumu/smartdns/commits | jq .[0].commit.committer.date | awk -F "T" '{print $1}' | sed 's/\"//g' | sed 's/\-/\./g'`)
+SMAERTDNS_SHA=$(echo -n `curl -sL https://api.github.com/repos/pymumu/smartdns/commits | jq .[0].sha | sed 's/\"//g'`)
+sed -i '/PKG_MIRROR_HASH:=/d' package/custom/smartdns/Makefile
+sed -i 's/PKG_VERSION:=.*/PKG_VERSION:='"$SMARTDNS_VER"'/g' package/custom/smartdns/Makefile
+sed -i 's/PKG_SOURCE_VERSION:=.*/PKG_SOURCE_VERSION:='"$SMAERTDNS_SHA"'/g' package/custom/smartdns/Makefile
+sed -i 's/PKG_VERSION:=.*/PKG_VERSION:='"$SMARTDNS_VER"'/g' package/custom/luci-app-smartdns/Makefile

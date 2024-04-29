ea5cd3f2a7ed33ea1642140e7b528e738346e491
hubbylei <hubbylei@hubbylei.dynu.net>
2024-04-30 04:20:49 +0800

    Fix Debug

--- diy-part1.sh
+++ diy-part1.sh
@@ -3,7 +3,7 @@
 git clone https://github.com/pymumu/openwrt-smartdns package/custom/smartdns
 git clone https://github.com/pymumu/luci-app-smartdns -b master package/custom/luci-app-smartdns
 git clone https://github.com/xiaorouji/openwrt-passwall -b main package/custom/luci-app-passwall
-git clone https://github.com/xiaorouji/openwrt-passwall-packages -b main package/custom/openwrt-passwall
+git clone https://github.com/xiaorouji/openwrt-passwall-packages -b main package/custom/passwall-packages
 git clone https://github.com/tty228/luci-app-wechatpush -b master package/custom/luci-app-wechatpush
 git clone https://github.com/hubbylei/openssl package/custom/openssl
 git clone https://github.com/hubbylei/wrtbwmon -b master package/custom/wrtbwmon

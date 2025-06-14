7c0f00d3c632040c32a5a76dcf0496ff8e098777
hubbylei <hubbylei@hubbylei.dynu.net>
2025-06-15 00:39:00 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -19,6 +19,7 @@ done
 
 cp -rf tmp/packages/lang/rust feeds/packages/lang/
 cp -rf tmp/luci/modules/luci-lua-runtime feeds/luci/modules/
+cp -rf tmp/luci/contrib/package/ucode-mod-lua feeds/luci/contrib/package/
 
 # golang
 git clone --depth 1 --single-branch https://github.com/sbwml/packages_lang_golang -b 24.x feeds/packages/lang/golang
